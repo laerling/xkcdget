813351f871e36a7b5f2be27447ff102e5af0acff
hkgit03 <22918836+hkgit03@users.noreply.github.com>
2018-02-13 19:55:32 +0100

    Turn lists into one source file
    No more unnecessary file operations

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,11 +8,6 @@ import (
 )
 
 func main() {
-	// read lists
-	nounsList := readList("nouns")
-	adjectivesList := readList("adjectives")
-	verbsList := readList("verbs")
-
 	// read key from stdin
 	input, err := ioutil.ReadAll(os.Stdin)
 	checkErr(err, "Cannot read key from stdin")
@@ -38,35 +32,11 @@ func checkErr(err error, msg string) {
 	}
 }
 
-//readList reads the file with name LISTNAME from the lists directory and
-//returns it as a slice
-func readList(listname string) []byte {
-	listDump, err := ioutil.ReadFile("lists/" + listname)
-	checkErr(err, "Cannot read file")
-	return listDump
-}
-
-//getLine returns a slice containing the line (delimited by '\n') which
-//contains the byte at position OFFSET in LIST
-//FIXME Don't accept empty lines, rename to getNonEmptyLine
-func getLine(list []byte, offset int) []byte {
-	begin := bytes.LastIndexByte(list[:offset], '\n') + 1
-	if begin < 0 {
-		begin = 0
-	}
-	var relEnd = bytes.IndexByte(list[offset:], '\n')
-	if relEnd < 0 {
-		relEnd = len(list)
-	}
-	end := offset + relEnd
-	return list[begin:end]
-}
-
 //chooseWordFromList chooses a word from LIST, determined by SEED
-func chooseWordFromList(list []byte, seed []byte) []byte {
+func chooseWordFromList(list []string, seed []byte) string {
 	seedScalar := 0
 	for _, b := range seed {
 		seedScalar += int(b) //TODO evaluate this
 	}
-	return getLine(list, seedScalar%len(list))
+	return list[seedScalar%len(list)]
 }
